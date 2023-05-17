6170fc86073f382848929568950fa3f19f3d49c9
Countryreid <117122319+Countryreid@users.noreply.github.com>
2023-05-17 08:39:15 -0600

    Added css for header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './header.css';
+
+import Header from './header';
 
 function App() {
   return (
-    <div className="App">
+    <section>
+    <div>{<Header></Header>}</div>
+    {/* <div className="App">
+    
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,7 +25,10 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+      
+
+    </div> */}
+    </section>
   );
 }
 
---
+++ src/header.jsx
@@ -0,0 +1,20 @@
+
+
+function Header(){
+    return(
+    <header className="header"> 
+    <h1>Open Blog Platform</h1>
+    <nav className="links">
+    
+        <a href="default.asp">Home</a>
+
+        <a href="bloggers.asp">Bloggers</a>
+        
+        <a href="contact.asp">Contact</a>
+        
+        <a href="about.asp">About</a>
+    </nav>
+    </header>
+    );
+}
+export default Header;
